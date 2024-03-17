a49506ca0d8242b95a5086fd2bba88a9716aee09
tusharchemate <tusharchemate07@gmail.com>
2024-03-17 12:38:38 +0530

    added init layout

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+const MarketingLayout = ({ children }: Props) => {
+  return (
+    <div className="min-h-screen flex flex-co">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
