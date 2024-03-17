9fc3b00141d472b8cf5842ba9a7e3d1548de4e75
tusharchemate <tusharchemate07@gmail.com>
2024-03-17 20:50:27 +0530

    Fixed user progress issue in user-progress

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { courses, userProgress } from "@/db/schema";
 import Card from "./card";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { upsertUserProgress } from "@/actions/user-progress";
 
 type ListProps = {
 courses:  typeof courses.$inferSelect[];
@@ -8,6 +13,21 @@ activeCourseId: typeof userProgress.$inferSelect.activeCourseId | any ,
 
 
 const List = ({courses, activeCourseId}: ListProps) => {
+
+  const router = useRouter();
+  const [pending, startTransition] = useTransition();
+
+  const onClick =(id:number) => {
+
+    if(pending) return;
+
+    if(id == activeCourseId) return router.push('/learn');
+
+    startTransition(()=>{
+      upsertUserProgress(id);
+    })
+  }
+
   return (
     <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill, minmax(210, 1fr))] gap-4">
       {courses?.map((course)=> (
@@ -16,8 +36,8 @@ const List = ({courses, activeCourseId}: ListProps) => {
             id={course.id}
             title={course.title}
             imageSrc={course.imageSrc}
-            onClick={()=> {}}
-            disabled={false}
+            onClick={onClick}
+            disabled={pending}
             active={course.id == activeCourseId}
         />
       ))}
