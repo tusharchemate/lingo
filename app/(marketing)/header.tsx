23dd0dfeb8de4de6edb9ca8f55393d97d88a54e2
tusharchemate <tusharchemate07@gmail.com>
2024-03-17 13:15:56 +0530

    added header image and loader in app

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,6 +1,21 @@
+import { ClerkLoading } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+
 const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+        </ClerkLoading>
+      </div>
       Header
     </header>
   );
