74444c88534629eb0a6c855a096ffd37f68b0607
tusharchemate <tusharchemate07@gmail.com>
2024-03-17 16:56:14 +0530

    added wrappers in app

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,13 @@
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+
 const LearnPage = () => {
-  return <div>Lets Learn</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper> Stciky Wrapper </StickyWrapper>
+      <FeedWrapper > Feed Wrapper </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
