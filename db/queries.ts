5d47193994cbef6a142ef5310dc745c97db50d06
tusharchemate <tusharchemate07@gmail.com>
2024-03-18 23:58:55 +0530

    added get units query

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "@/db/drizzle";
 import { auth } from "@clerk/nextjs";
-import { courses, userProgress } from "./schema";
+import { challengeProgress, courses, units, userProgress } from "./schema";
 import { eq } from "drizzle-orm";
 
 
@@ -26,6 +26,61 @@ export const getUserProgress = cache(async ()=> {
     });
     return data;
 });
+
+export const getUnits = cache(async () => {
+    const { userId } = await auth();
+    const userProgress = await getUserProgress();
+  
+    if (!userId || !userProgress?.activeCourseId) {
+      return [];
+    }
+  
+    const data = await db.query.units.findMany({
+      orderBy: (units, { asc }) => [asc(units.order)],
+      where: eq(units.courseId, userProgress.activeCourseId),
+      with: {
+        lessons: {
+          orderBy: (lessons, { asc }) => [asc(lessons.order)],
+          with: {
+            challenges: {
+              orderBy: (challenges, { asc }) => [asc(challenges.order)],
+              with: {
+                challengeProgress: {
+                  where: eq(
+                    challengeProgress.userid,
+                    userId,
+                  ),
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+  
+    const normalizedData = data.map((unit) => {
+      const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+        if (
+          lesson.challenges.length === 0
+        ) {
+          return { ...lesson, completed: false };
+        }
+  
+        const allCompletedChallenges = lesson.challenges.every((challenge) => {
+          return challenge.challengeProgress
+            && challenge.challengeProgress.length > 0
+            && challenge.challengeProgress.every((progress) => progress.completed);
+        });
+  
+        return { ...lesson, completed: allCompletedChallenges };
+      });
+  
+      return { ...unit, lessons: lessonsWithCompletedStatus };
+    });
+  
+    return normalizedData;
+  });
+  
 
 export const getCourseById = cache(async (courseId:number)=> {
 
