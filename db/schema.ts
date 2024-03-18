96da2a78bfba5babae48df7401a61a862349d340
tusharchemate <tusharchemate07@gmail.com>
2024-03-18 21:23:45 +0530

    added lesson and units schema

--- db/schema.ts
+++ db/schema.ts
@@ -7,25 +7,50 @@ export const courses = pgTable("courses", {
   imageSrc: text("image_src").notNull(),
 });
 
-
-export const coursesRelations = relations(courses, ({many}) => ({
-    userProgress:many(userProgress),
+export const coursesRelations = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+  units: many(units),
 }));
 
+export const units = pgTable("units", {
+  id: serial("id").primaryKey(),
+  title: text("text").notNull(),
+  description: text("description").notNull(),
+  courseId: integer("course_id")
+    .references(() => courses.id, { onDelete: "cascade" })
+    .notNull(),
+  order: integer("order").notNull(),
+});
+
+const unitsRelations = relations(units, ({ many, one }) => ({
+  course: one(courses, {
+    fields: [units.courseId],
+    references: [courses.id],
+  }),
+}));
 
 export const userProgress = pgTable("user_progress", {
   userId: text("user_id").primaryKey(),
   userName: text("user_name").notNull().default("User"),
   userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
-  activeCourseId: integer("active_course_id").references(()=> courses.id, {onDelete:"cascade"}),
-  hearts: integer('hearts').notNull().default(5),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(5),
   points: integer("points").notNull().default(0),
 });
 
-
-export const userProgressRelations = relations(userProgress, ({one})=> ({
-    activeCourse: one(courses, {
-        fields:[userProgress.activeCourseId],
-        references:[courses.id],
-    }),
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
 }));
+
+
+export const lessons = pgTable('lessons', {
+  id:serial('id').primaryKey(),
+  title: text('title').notNull(),
+  unitId: integer('unit_id').references(()=> units.id, {onDelete:'cascade'}).notNull(),
+  order:integer('order').notNull(),
+});
