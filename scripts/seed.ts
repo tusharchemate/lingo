b2558157360b083d860febaeec29eb4ce2fd063e
tusharchemate <tusharchemate07@gmail.com>
2024-03-18 20:38:52 +0530

    added db scripts for seeding

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -8,14 +8,43 @@ const sql = neon(process.env.DATABASE_URL!);
 //@ts-ignore
 const db = drizzle(sql, { schema });
 
+
+const languages = [
+  {
+    id:1,
+    title:'Indian',
+    imageSrc:'/in.png',
+  },{
+    id:2,
+    title:'Japanese',
+    imageSrc:'/jp.svg',
+  },
+  {
+    id:3,
+    title:'Spanish',
+    imageSrc:'/es.svg',
+  },
+  {
+    id:4,
+    title:'Russian',
+    imageSrc:'/fr.svg',
+  }
+];
+
+
 const main = async () => {
   try {
     console.log("Seeding Database...");
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+
+    await db.insert(schema.courses).values(languages);
+
     console.log("Seeding Finished!");
   } catch (err) {
     console.log(err);
     throw new Error("Failed to Seed database");
   }
 };
+
+main();
