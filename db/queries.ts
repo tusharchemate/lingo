1be7642196bf03b400828f73955b852046005c98
tusharchemate <tusharchemate07@gmail.com>
2024-03-20 20:43:41 +0530

    added getLesson in queries

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "@/db/drizzle";
 import { auth } from "@clerk/nextjs";
-import { challengeProgress, courses, units, userProgress } from "./schema";
+import { challengeProgress, courses, lessons, units, userProgress } from "./schema";
 import { eq } from "drizzle-orm";
 
 
@@ -135,3 +135,49 @@ export const getCourseProgress = cache(async ()=> {
   };
 
 })
+
+
+export const getLesson = cache(async (id?: number) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const courseProgress = await getCourseProgress();
+
+  const lessonId = id || courseProgress?.activeLessonId;
+
+  if (!lessonId) {
+    return null;
+  }
+
+  const data = await db.query.lessons.findFirst({
+    where: eq(lessons.id, lessonId),
+    with: {
+      challenges: {
+        orderBy: (challenges, { asc }) => [asc(challenges.order)],
+        with: {
+          challengeOptions: true,
+          challengeProgress: {
+            where: eq(challengeProgress.userid, userId),
+          },
+        },
+      },
+    },
+  });
+
+  if (!data || !data.challenges) {
+    return null;
+  }
+
+  const normalizedChallenges = data.challenges.map((challenge) => {
+    const completed = challenge.challengeProgress 
+      && challenge.challengeProgress.length > 0
+      && challenge.challengeProgress.every((progress) => progress.completed)
+
+    return { ...challenge, completed };
+  });
+
+  return { ...data, challenges: normalizedChallenges }
+});
